10f8b61649083f2de87e417254879897ede5dcf4
Zoe Slendebroek <zoefleurslendebroek@gmail.com>
2024-11-09 14:17:34 -0500

    SpellChecker Methods Created

--- SpellChecker.java
+++ SpellChecker.java
@@ -1,14 +1,94 @@
+import java.io.FileInputStream;     // Import FileInputStream to read file
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;           // Import the Scanner class
+
+
 public class SpellChecker {
 
     public SpellChecker() {
       // TODO: You can modify the body of this constructor,
       // or you can leave it blank. You must keep the signature, however.
     }
+
+
+    /* ////////////////////////////////////////////// */
+    /* ////////////////////////////////////////////// */
+    /* MAIN METHOD: JUST FOR ME TO CHECK AND RUN CODE */
+    public static void main(String[] args) {
+        SpellChecker.start();
+    }
+    /* ////////////////////////////////////////////// */
+    /* ////////////////////////////////////////////// */
+    /* ////////////////////////////////////////////// */
+
+
   
     public static void start() {
-      // TODO: You can modify the body of this method,
-      // or you can leave it blank. You must keep the signature, however.
+      // TODO: You can modify the body of this method
+
+        Scanner userInput = new Scanner(System.in);                 // Initializes Scanner and passes it as an argument to other methods
+
+        FileInputStream dictionary = openDictionary(userInput);     // Prompts user for Dictionary input
+        FileInputStream file = openSpellCheckFile(userInput);           // Prompts user for File input
+
+
+        //  3.) Iterate Over Words: Read each word from the input file and check against words in dictionary
+        //  4.) For Each misspelled word, prompt the user with options to accept, replace, or enter a correction
+        //  5.) After correction, write the corrected words to a new output file, as specified
+
+        userInput.close();                                          // Closes Scanner
     }
-  
-    // You can of course write other methods as well.
+
+
+
+    // Method to open user input Dictionary
+    private static FileInputStream openDictionary(Scanner scanner) {
+        String userDictName;                                            // Local Variable that stores user Input
+
+        while (true) {                                                  // While Loop continues until User enters valid input Dictionary
+            System.out.printf(Util.DICTIONARY_PROMPT);                  // Prompts user to input Dictionary file
+            userDictName = scanner.nextLine();                        // Stores User input in 'userDictName'
+
+
+            try {                                                       // try/catch to see if Dictionary file is valid and can be opened
+                FileInputStream input = new FileInputStream(userDictName);
+                System.out.printf(Util.DICTIONARY_SUCCESS_NOTIFICATION, userDictName);
+                return input;
+            } catch (IOException e) {                                   // Throws IO Exception if File is not valid and prompts user to try again
+                System.out.printf(Util.FILE_OPENING_ERROR);
+            }
+
+        }
+
+    }
+
+
+    // Method to open user input File to SpellCheck
+    private static FileInputStream openSpellCheckFile(Scanner scanner) {
+        String userFileName;                                // Local Variable that stores user Input
+
+        while (true) {
+            System.out.printf(Util.FILENAME_PROMPT);
+            userFileName = scanner.nextLine(); // Update userDictName with new input
+
+            try {
+                FileInputStream input = new FileInputStream(userFileName);
+                System.out.printf(Util.FILE_SUCCESS_NOTIFICATION, userFileName);
+                return input;
+            } catch (IOException e) {
+                System.out.printf(Util.FILE_OPENING_ERROR);
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
   }
--- WordRecommender.java
+++ WordRecommender.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 public class WordRecommender {
 
     public WordRecommender(String dictionaryFile) {    
