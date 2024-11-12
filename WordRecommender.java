90f4b519ef4b9116d8f395b433ad90fbbabd0fc5
katelynvo <katelyn.vo@gmail.com>
2024-11-12 10:05:50 -0500

    getSimilarity method

--- WordRecommender.java
+++ WordRecommender.java
@@ -21,8 +21,26 @@ public class WordRecommender {
     }
   
     public double getSimilarity(String word1, String word2) {
-      // TODO: change this!
-      return 0.0;
+        int left = 0;
+        int right = 0;
+        int minLength = Math.min(word1.length(), word2.length());
+
+        // Calculate left similarity
+        for (int i = 0; i < minLength; i++) {
+            if (word1.charAt(i) == word2.charAt(i)) {
+                left++;
+            }
+        }
+
+        // Calculate right similarity
+        for (int i = 1; i <= minLength; i++) {
+            if (word1.charAt(word1.length() - i) == word2.charAt(word2.length() - i)) {
+                right++;
+            }
+        }
+
+        // Return average of left and right similarity
+        return (left + right) / 2.0;
     }
   
     public ArrayList<String> getWordSuggestions(String word, int tolerance, double commonPercent, int topN) {
