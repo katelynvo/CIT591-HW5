1622b41b067f19e2376f4f395be4fdc17b35374b
katelynvo <katelyn.vo@gmail.com>
2024-11-12 09:59:21 -0500

    WordRecommender method

--- WordRecommender.java
+++ WordRecommender.java
@@ -1,7 +1,23 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
 public class WordRecommender {
 
-    public WordRecommender(String dictionaryFile) {    
-      // TODO: change this!
+    private Set<String> dictionary;
+
+    public WordRecommender(String dictionaryFile) {
+        dictionary = new HashSet<>();
+        try (Scanner scanner = new Scanner(new File(dictionaryFile))) {
+            while (scanner.hasNextLine()) {
+                dictionary.add(scanner.nextLine().trim().toLowerCase());
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("Dictionary file not found.");
+            e.printStackTrace();
+        }
     }
   
     public double getSimilarity(String word1, String word2) {
